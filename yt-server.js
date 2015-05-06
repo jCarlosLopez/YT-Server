5f44c87781d9d814e45043f3f0bf76ace17c7a22
Jose Luis Jimenez <joseluiszero@gmail.com>
2015-05-06 10:14:25 +0200

    Server working

--- yt-server.js
+++ yt-server.js
@@ -1,9 +1,44 @@
-var http = require('http');
+var express = require('express');
+//Express Web Server
+var busboy = require('connect-busboy');
+//middleware for form/file upload
+var path = require('path');
+//used for file path
+var fs = require('fs-extra');
+//File System - for file manipulation
+
+var app = express();
+app.use(busboy());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-http.createServer(function (req, res) {
+app.route('/upload').get(function(req, res, next) {
+  
+});
 
+/* ==========================================================
+ Create a Route (/upload) to handle the Form submission
+ (handle POST requests to /upload)
+ Express v4  Route definition
+ ============================================================ */
+app.route('/upload').post(function(req, res, next) {
 
-}).listen(8080, '0.0.0.0');
+  var fstream;
+  req.pipe(req.busboy);
+  req.busboy.on('file', function(fieldname, file, filename) {
+    console.log("Uploading: " + filename);
 
-console.log('Server running at http://192.168.3.1:8080/');
+    //Path where image will be uploaded
+    fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
+    file.pipe(fstream);
+    fstream.on('close', function() {
+      console.log("Upload Finished of " + filename);
+      res.redirect('back');
+      //where to go next
+    });
+  });
+});
+
+var server = app.listen(3030, function() {
+  console.log('Listening on port %d', server.address().port);
+}); 
